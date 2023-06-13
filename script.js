6289594d459711dcf93c3c82e711fe16dd960926
Michael Pinkston <mkpinkston@my.milligan.edu>
2023-06-13 15:05:57 -0400

    Created Reset Button. Created prompt button to get users desired grid size, but have not implimented user response.

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const grid = document.querySelector("#grid");
 console.log(grid);
-// create div cells
+
 createDivs();
 
+const cells = document.getElementsByClassName("cell");
+addEventListenerList(cells, "mouseover");
+
+const updateBtn = document.querySelector("#updateBtn");
+updateBtn.addEventListener("click", gridPrompt);
+
+const resetBtn = document.querySelector("#resetBtn");
+resetBtn.addEventListener("click", resetDivs);
+
+function addEventListenerList(cells, event) {
+  for (let i = 0, len = cells.length; i < len; i++) {
+    cells[i].addEventListener(event, function () {
+      cells[i].style.backgroundColor = "black";
+    });
+  }
+}
+
+// create div cells
 function createDivs() {
   for (let i = 0; i < 256; i++) {
     const cell = document.createElement("div");
-    console.log(cell);
-    console.log(grid);
     cell.classList.add("cell");
     cell.setAttribute("id", `${i}`);
     //   cell.textContent = "square";
@@ -15,4 +31,21 @@ function createDivs() {
   }
 }
 
-//
+// Prompt user for desired grid size
+function gridPrompt() {
+  let gridSize = window.prompt(
+    "What size grid do you want to use? Choose a whole number between 1 and 100.",
+    16
+  );
+  let num = parseInt(gridSize);
+  return num;
+}
+
+// reset background colors of cells
+function resetDivs() {
+  const divs = document.querySelectorAll(".cell");
+
+  for (let i = 0, len = divs.length; i < len; i++) {
+    divs[i].style.backgroundColor = "";
+  }
+}
